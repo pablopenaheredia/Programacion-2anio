7fe0c1e39f49135371a39fb6d31900f03ecfb4a0
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2024-10-13 22:53:54 -0300

    Update matrices_tkinter.py

--- Estadistica2/matrices_tkinter.py
+++ Estadistica2/matrices_tkinter.py
@@ -18,11 +18,17 @@ def obtener_matriz_y_resultados():
             matriz.append(fila)
             resultados.append(float(entry_resultados[i].get()))
 
+        # Validar si alguna fila es nula
+        for fila in matriz:
+            if all(element == 0 for element in fila):
+                messagebox.showerror("Error", "Una fila no puede ser nula (todos los elementos son cero).")
+                return
+
         soluciones = gauss_jordan(matriz, resultados)
         if soluciones is not None:
             mensaje = "Sistema Compatible Determinado\n"
             for i in range(n):
-                mensaje += f"x{i+1} = {round(soluciones[i], 4)}\n"
+                mensaje += f"x{chr(0x2081)} = {round(soluciones[i], 4)}\n"
             messagebox.showinfo("Resultados", mensaje)
     except ValueError:
         messagebox.showerror("Error", "Debes ingresar números válidos.")
@@ -44,25 +50,25 @@ def crear_entradas():
         for i in range(n):
             fila = []
             for j in range(n):
-                label = tk.Label(frame_matriz, text=f"a{i+1}{j+1}:")
+                label = tk.Label(frame_matriz, text=f"a{chr(0x2081)}{chr(0x2081 + j)}:")
                 label.grid(row=i, column=j*2, padx=5, pady=5)
                 entry = tk.Entry(frame_matriz)
                 entry.grid(row=i, column=j*2+1, padx=5, pady=5)
                 fila.append(entry)
             entry_matriz.append(fila)
-            label_resultado = tk.Label(frame_matriz, text=f"b{i+1}:")
+            label_resultado = tk.Label(frame_matriz, text=f"b{chr(0x2081 + i)}:")
             label_resultado.grid(row=i, column=n*2, padx=5, pady=5)
             entry_resultado_i = tk.Entry(frame_matriz)
             entry_resultado_i.grid(row=i, column=n*2+1, padx=5, pady=5)
             entry_resultados.append(entry_resultado_i)
     except ValueError:
         messagebox.showerror("Error", "Debes ingresar un número entero válido para el tamaño de la matriz.")
-
+    
 root = tk.Tk()
 root.title("Método de Gauss-Jordan")
 root.configure(bg="#f0f0f0")
 
-label_n = tk.Label(root, text="Tamaño de la matriz (n):", bg="#f0f0f0")
+label_n = tk.Label(root, text="Tamaño de la matriz (nxn) Debe ser cuadrada:", bg="#f0f0f0")
 label_n.grid(row=0, column=0, padx=10, pady=10)
 
 entry_n = tk.Entry(root, width=5)
@@ -77,7 +83,7 @@ frame_matriz.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 boton_calcular = tk.Button(root, text="Calcular", command=obtener_matriz_y_resultados, bg="#cccccc")
 boton_calcular.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
 
-# Make the GUI responsive
+# Hacer la GUI responsiva
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
